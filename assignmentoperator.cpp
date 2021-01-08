e8ffe3ef5628e240859824dd9c0924c5ae0a6e12
virajj014 <76771864+virajj014@users.noreply.github.com>
2021-01-08 14:12:03 +0530

    Add files via upload

---
+++ assignmentoperator.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int myNum = 15 ;
+    myNum += 20;
+    cout <<myNum;
+    return 0;
+}
+
+
---
+++ logicaloperators.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x=5;
+    cout <<!(x>1 && x>7);
+    return 0;
+}
+
+
+
+
+
+
+
